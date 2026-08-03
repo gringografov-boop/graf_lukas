841fb0e34ab17842d16bb636577574a7a85f093f
gringografov-boop <gringo.grafov@gmail.com>
2026-08-03 20:06:37 +0300

    update supobase config

--- docs/assets/data/supabase-config.js
+++ docs/assets/data/supabase-config.js
@@ -1,4 +1,4 @@
 window.GRAF_LUKAS_SUPABASE_CONFIG = {
-  url: "https://kyjrztsmayopkstwjdtj.supabase.co/rest/v1/",
+  url: "https://kyjrztsmayopkstwjdtj.supabase.co",
   publishableKey: "sb_publishable_0ItHJ0NebipG3WrBzYMMcg_SyUg9knO"
 };
